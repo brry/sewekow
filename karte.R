ae44a6b2072b4150ec8dd4e0a71fb1c4017d9d05
brry <berryboessenkool@hotmail.com>
2021-04-05 19:00:34 +0200

    updates

--- karte.R
+++ karte.R
@@ -5,20 +5,24 @@
 library(leaflet)
 library(leaflet.extras)
 
-if(!exists("shp2")){
+if(!exists("shp")){
 message("Reading data...")
 shp <- sf::st_read("ALKIS_Shape_OPR/flurstueck.shp")
-lims <- c(xmin=338000, ymin=5895000, xmax=350000, ymax=5904000)
+}
+lims <- c(xmin=339400, ymin=5897700, xmax=350500, ymax=5904000)
 shp2 <- sf::st_crop(shp, lims) ; rm(lims)
 message("Projecting data...")
 shp2 <- sf::st_transform(shp2, '+proj=longlat +datum=WGS84')
-popup <- unname(berryFunctions::popleaf(shp2[,]))
-}
-
+shp2$flaeche <- shp2$flaeche/1e4
+cent <- sf::st_centroid(shp2)
+cent <- cent[cent$flaeche > 1.5,]
+popup <- unname(berryFunctions::popleaf(shp2, sel=c("gemarkung", "flur", "flurstnr", "aktualit", "flaeche", "lagebeztxt")))
+  
 message("Creating map...")
 # create map, add controls:
 rmap <- leaflet() %>% addPolygons(data=shp2, popup=popup, group="Flaechen", fill=FALSE) %>% #, 
         #  label=~flurstnr, labelOptions = labelOptions(noHide=T, textOnly=T, minZoom=13)) %>% 
+  addLabelOnlyMarkers(data=cent, label=~flurstnr, labelOptions=labelOptions(noHide=T, textOnly=T) ) %>% 
   addSearchOSM(options=searchOptions(autoCollapse=TRUE, minLength=2, hideMarkerOnCollapse=TRUE, zoom=16)) %>% 
   addControlGPS(options=gpsOptions(position="topleft", 
                                    activate=TRUE, autoCenter=TRUE, maxZoom=16, setView=TRUE)) %>% 
