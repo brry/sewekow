a70ec646c109c47108d1702e38f29be3c2923c93
brry <berryboessenkool@hotmail.com>
2021-04-05 18:04:21 +0200

    Karte added

---
+++ karte.R
@@ -0,0 +1,51 @@
+# ALKIS Karte
+# Berry, 2021-04-05
+# https://data.geobasis-bb.de/geobasis/daten/alkis/Vektordaten/shape/
+
+library(leaflet)
+library(leaflet.extras)
+
+if(!exists("shp2")){
+message("Reading data...")
+shp <- sf::st_read("ALKIS_Shape_OPR/flurstueck.shp")
+lims <- c(xmin=338000, ymin=5895000, xmax=350000, ymax=5904000)
+shp2 <- sf::st_crop(shp, lims) ; rm(lims)
+message("Projecting data...")
+shp2 <- sf::st_transform(shp2, '+proj=longlat +datum=WGS84')
+popup <- unname(berryFunctions::popleaf(shp2[,]))
+}
+
+message("Creating map...")
+# create map, add controls:
+rmap <- leaflet() %>% addPolygons(data=shp2, popup=popup, group="Flaechen", fill=FALSE) %>% #, 
+        #  label=~flurstnr, labelOptions = labelOptions(noHide=T, textOnly=T, minZoom=13)) %>% 
+  addSearchOSM(options=searchOptions(autoCollapse=TRUE, minLength=2, hideMarkerOnCollapse=TRUE, zoom=16)) %>% 
+  addControlGPS(options=gpsOptions(position="topleft", 
+                                   activate=TRUE, autoCenter=TRUE, maxZoom=16, setView=TRUE)) %>% 
+  addMeasure(primaryLengthUnit="kilometers", primaryAreaUnit="hectares",
+             activeColor="#3D535D", completedColor="#7D4479", position="topleft") %>% 
+  addScaleBar(position="topleft") %>% 
+  addFullscreenControl()
+# add background map layer options:
+prov <- c(OSM="OpenStreetMap", Sat="Esri.WorldImagery", Topo="OpenTopoMap") # mapview::mapviewGetOption("basemaps")
+for(pr in names(prov)) rmap <- rmap %>% addProviderTiles(unname(prov[pr]), group=pr, 
+                                                         options=providerTileOptions(maxZoom=20))
+rmap <- rmap %>% addLayersControl(baseGroups=names(prov),
+                                  overlayGroups=c("Flaechen"),
+                                  options=layersControlOptions(collapsed=FALSE))
+print(rmap)
+
+# Export:
+if(T){
+message("Exporting map as html...")
+htmlwidgets::saveWidget(rmap, "index.html", selfcontained=TRUE)
+message("Changing html header...")
+# HTML head for mobile devices:
+# https://stackoverflow.com/questions/42702394/make-leaflet-map-mobile-responsive
+map_h <- readLines("index.html")
+map_h <- sub('<title>leaflet</title>', x=map_h,
+             '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>\n<title>Flurkarte</title>')
+writeLines(map_h, "index.html") ; rm(map_h)
+berryFunctions::openFile("index.html")
+}  
+
